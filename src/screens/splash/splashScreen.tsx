c27a5f43b52b5b17a2faec3f21d1808ebcd80b81
Boris Roberto Sonwa Sonwa <sonwaroberto@gmail.com>
2024-02-29 10:45:28 +0100

    update

--- src/screens/splash/splashScreen.tsx
+++ src/screens/splash/splashScreen.tsx
@@ -2,8 +2,7 @@ import React, {FC, useEffect} from 'react';
 import {View, Image, ActivityIndicator} from 'react-native';
 import theme from '../../resources/theme';
 import styles from './splash.style';
-import {useAppDispatch, useAppSelector} from '../../redux/typings';
-import {RootState} from '../../redux/store';
+import {useAppDispatch} from '../../redux/typings';
 import storage from '../../utils/storage';
 import {setToken, setUser} from '../../features/auth/auth.slice';
 
@@ -13,15 +12,12 @@ type Props = {
 
 const SplashScreen: FC<Props> = ({navigation}) => {
   const dispatch = useAppDispatch();
-  const {user: User, accessToken} = useAppSelector(
-    (state: RootState) => state.authSlice,
-  );
 
   useEffect(() => {
     const loadUserData = async () => {
       try {
-        const token = await storage.load({key: 'token'});
-        const user = await storage.load({key: 'user'});
+        const token = await storage.remove({key: 'token'});
+        const user = await storage.remove({key: 'user'});
 
         dispatch(setToken(token));
         dispatch(setUser(user));
@@ -29,7 +25,9 @@ const SplashScreen: FC<Props> = ({navigation}) => {
         if (user && token) {
           navigation.navigate('Main Stack');
         } else {
-          navigation.replace('Onboarding');
+          setTimeout(() => {
+            navigation.replace('Onboarding');
+          }, 3000);
         }
       } catch (error) {
         console.error('Error loading user data:', error);
